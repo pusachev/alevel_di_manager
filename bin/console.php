951ac2dc88f82aa5abe43f3859dc3a240b3d3b1b
Pavel Usachev <pavel.usachev@balanceinternet.com.au>
2019-12-27 19:30:57 +0200

    fix error

--- bin/console.php
+++ bin/console.php
@@ -7,7 +7,7 @@ $factory = new \Pusachev\ObjectManager\Factory\Factory();
 $objectManager = new \Pusachev\ObjectManager\ObjectManager($factory);
 
 /** @var \Pusachev\ObjectManager\Test\TestDTO $dto */
-$dto = $objectManager->create(\Pusachev\ObjectManager\Test\TestDTO::class, ['data' => [2 => 'two']]);
+$dto = $objectManager->create(\Pusachev\ObjectManager\Test\TestDTO::class);
 
 $dto->set(1, 'test');
 
